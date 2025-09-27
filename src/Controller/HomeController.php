768bd946d8daf38ac9ea3757e7a92d9a64a9272a
desss <dwen.studacc@gmail.com>
2025-09-27 13:08:37 +0800

    Landing page added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,6 +10,21 @@ class HomeController extends AbstractController {
     #[Route('/')]
     public function index():Response {
 
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+    'services' => [
+        ['title' => 'Bath & Blow Dry', 'desc' => 'Keep your pet fresh and clean', 'link' => '#'],
+        ['title' => 'Full Groom', 'desc' => 'Complete haircut and styling', 'link' => '#'],
+        ['title' => 'Spa Treatment', 'desc' => 'De-shedding and skin care', 'link' => '#'],
+    ],
+    'packages' => [
+        ['name' => 'Essential Care', 'price' => '₱800', 'features' => ['Bath', 'Nail Trim']],
+        ['name' => 'Complete Groom', 'price' => '₱1600', 'features' => ['Haircut', 'Bath', 'Ear Cleaning']],
+    ],
+    'reviews' => [
+        ['author' => 'Maria D.', 'stars' => 5, 'comment' => 'Great service, my dog loved it!'],
+        ['author' => 'John P.', 'stars' => 4, 'comment' => 'Easy booking and friendly staff.'],
+    ]
+]);
+
     }
 }
